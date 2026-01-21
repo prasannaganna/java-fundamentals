7cbe295cc2445d423f6cf0c1ef6279e55c93ec8b
prasannaganna <prasannaganna1216@gmail.com>
2026-01-22 00:28:24 +0530

    feat:completed task

--- src/com/st/java/fundamentals/Primitives.java
+++ src/com/st/java/fundamentals/Primitives.java
@@ -1,14 +1,49 @@
 package com.st.java.fundamentals;
 
-public class Primitives {
+public class Primitives
+{
     static void main(String[] args) {
-        byte age = 23;
-        System.out.println("age : "+ age);
-        short silver = 300;
-        System.out.println("silver : "+ silver);
-        int salary = 3500;
+        byte age, months, daysofMonth, temperature, rollNo;
+        age = 23;
+        months = 12;
+        daysofMonth = 24;
+        temperature = -10;
+        rollNo = 10;
+        System.out.println("age : " + age);
+        System.out.println("months : " + months);
+        System.out.println("daysofMonth : " + daysofMonth);
+        System.out.println("temperature :" + temperature);
+        System.out.println("rollNo :" + rollNo);
+        short currentYear, fourDigitPinNumber, jeeRank, bikeNo,simpleShopping;
+        currentYear = 2016;
+        fourDigitPinNumber = 5478;
+        jeeRank = 356;
+        bikeNo = 2345;
+        simpleShopping = 23567 ;
+        System.out.println("currentYear : " + currentYear);
+        System.out.println("fourDigitPinNumber: " + fourDigitPinNumber);
+        System.out.println("jeeRank : " + jeeRank);
+        System.out.println("bikeNo : " + bikeNo);
+        System.out.println("simpleShopping : " + simpleShopping);
+        int salary,hyderabadPopulation,pinCode,priceofIPhone,secondsinaDay;
+        salary = 25000;
+        hyderabadPopulation = 4567889;
+        pinCode = 517424;
+        priceofIPhone = 200000 ;
+        secondsinaDay = 86000 ;
         System.out.println("salary : " + salary);
-        long number = 1234567889;
-        System.out.println("number :" + number);
+        System.out.println("hyderabadPopulation : " + hyderabadPopulation);
+        System.out.println("pinCode : " + pinCode);
+        System.out.println("priceofIPhone : " +priceofIPhone);
+        System.out.println("secondsinaDay : " + secondsinaDay);
+        long worldPopulation,adhaarNumber,phoneNumber,nationBudget;
+        worldPopulation = 8100000000000L;
+        adhaarNumber = 555566667777l;
+        phoneNumber = 7569313517L;
+        nationBudget = 4800000000000l;
+        System.out.println("worldPopulation : " + worldPopulation);
+        System.out.println("adhaarNumber : " + adhaarNumber);
+        System.out.println("phoneNumber : " + phoneNumber);
+        System.out.println("nationBudget : " + nationBudget);
     }
 }
