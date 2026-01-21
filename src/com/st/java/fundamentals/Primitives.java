3e6aec597bb088b5694816efa1312064ea938c9a
prasannaganna <prasannaganna1216@gmail.com>
2026-01-21 16:04:01 +0530

    feat:first commit

---
+++ src/com/st/java/fundamentals/Primitives.java
@@ -0,0 +1,14 @@
+package com.st.java.fundamentals;
+
+public class Primitives {
+    static void main(String[] args) {
+        byte age = 23;
+        System.out.println("age : "+ age);
+        short silver = 300;
+        System.out.println("silver : "+ silver);
+        int salary = 3500;
+        System.out.println("salary : " + salary);
+        long number = 1234567889;
+        System.out.println("number :" + number);
+    }
+}
